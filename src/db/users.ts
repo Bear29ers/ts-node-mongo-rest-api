e1bf73930f70a55483a8db9663532c1c8f701b26
Yuta Okuma <y.dream.nest@icloud.com>
2023-10-21 11:26:25 +0900

    refactor: :bulb: refactor userschema

--- src/db/users.ts
+++ src/db/users.ts
@@ -5,9 +5,9 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   authentication: {
     password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
   },
-  salt: { type: String, selct: false },
-  sessionToken: { type: String, select: false },
 });
 
 export const UserModel = mongoose.model('User', UserSchema);
