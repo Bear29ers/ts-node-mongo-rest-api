34c5a8d1592f8afcdf1e917b7d127e0e3b470747
Yuta Okuma <y.dream.nest@icloud.com>
2023-10-31 21:10:14 +0900

    feat: :sparkles: create middleware

---
+++ src/middleware/index.ts
@@ -0,0 +1,26 @@
+import type express from 'express';
+import { get, merge } from 'lodash';
+
+import { getUserBySessionToken } from '../db/users';
+
+export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    const sessionToken = req.cookies['BEAR29ERS-AUTH'];
+
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+
+    const existingUser = await getUserBySessionToken(sessionToken);
+
+    if (!existingUser) {
+      return res.sendStatus(403);
+    }
+
+    merge(req, { identity: existingUser });
+
+    return next();
+  } catch (error) {
+    return res.sendStatus(400);
+  }
+};
